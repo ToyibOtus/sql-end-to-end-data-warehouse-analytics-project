88672cfd238dc9cc997d3799a9fcc1dcada28b05
Toyib Otusanya <toyibotusanya@gmail.com>
2026-01-23 16:35:16 +0000

    Update 00_database_exploration.sql

--- scripts(data analytics)/01_Exploratory_data_analytics/00_database_exploration.sql
+++ scripts(data analytics)/01_Exploratory_data_analytics/00_database_exploration.sql
@@ -1,17 +1,27 @@
 /*
-====================================================================================
+===================================================================
 Database & Objects Exploration
-====================================================================================
+===================================================================
 Script Purpose:
-	This script explores all relevant objects in database [SalesDatabase].
-====================================================================================
+	This script explores all relevant objects in [SalesDatabase].
+	It aims to identify the number of relevant objects, and their
+	respective fields.
+===================================================================
 */
 USE SalesDatabase;
 GO
 
+-- Explore gold objects in Salesdatabase
 SELECT * FROM INFORMATION_SCHEMA.TABLES
 WHERE TABLE_SCHEMA = 'gold';
 
-
-SELECT TABLE_SCHEMA, TABLE_NAME, COLUMN_NAME, ORDINAL_POSITION FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'gold' ORDER BY TABLE_NAME, ORDINAL_POSITION;
+-- Explore columns in each gold object
+SELECT 
+	TABLE_CATALOG, 
+	TABLE_SCHEMA, 
+	TABLE_NAME, 
+	COLUMN_NAME, 
+	ORDINAL_POSITION 
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'gold'
+ORDER BY TABLE_NAME, ORDINAL_POSITION;
