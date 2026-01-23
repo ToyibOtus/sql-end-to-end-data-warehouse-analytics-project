f39ae5f29640860f77ad5bd28b6b033e927cfffd
Toyib Otusanya <toyibotusanya@gmail.com>
2026-01-23 16:48:59 +0000

    Correct test run folder name in SQL procedure
    Updated the note regarding the test run folder for clarity.

--- scripts(data eng)/03_silver/01_proc_load_customers.sql
+++ scripts(data eng)/03_silver/01_proc_load_customers.sql
@@ -16,7 +16,7 @@ Note:
 	* Running this script independently demands that you assign an integer value to @job_run_id.
 	* It is imperative that this value already exist in the log table [metadata.etl_job_run] due
 	  to the foreign key constraint set on dependent tables.
-	* To test the working condition of this script, check folder titled "test_run".
+	* To test the working condition of this script, check folder titled "test_run_silver".
 =======================================================================================================
 */
 CREATE OR ALTER PROCEDURE silver.usp_load_silver_customers @job_run_id INT AS
