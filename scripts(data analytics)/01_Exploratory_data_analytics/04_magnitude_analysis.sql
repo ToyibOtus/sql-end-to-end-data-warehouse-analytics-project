dd82330c8b9cdae40794126558a0d11c38b0e438
Toyib Otusanya <toyibotusanya@gmail.com>
2026-01-23 16:37:20 +0000

    Update 04_magnitude_analysis.sql

--- scripts(data analytics)/01_Exploratory_data_analytics/04_magnitude_analysis.sql
+++ scripts(data analytics)/01_Exploratory_data_analytics/04_magnitude_analysis.sql
@@ -1,146 +1,299 @@
 /*
-=======================================================================================
+========================================================================================
 Magnitude Analysis
-=======================================================================================
+========================================================================================
 Script Purpose:
-	This script performs magnitude analysis. It retrieves insights into how important
-	business metrics are distributed across relevant dimensions.
-=======================================================================================
+	 This script performs magnitude analysis. It retrieves data that shows insight into
+	 how key business metrics are distributed across relevant dimensions.
+========================================================================================
 */
--- Which country generates the highest profit, 
--- and what other underlying factors contribute to this performance?  
+-- Which country generates the highest revenue
+-- and what other metrics are at play?
 SELECT
 	dc.country,
 	COUNT(DISTINCT dc.customer_key) AS total_customers,
 	COUNT(DISTINCT fo.customer_key) AS total_customers_ordered,
-	(CAST(COUNT(DISTINCT fo.customer_key) AS FLOAT))/COUNT(DISTINCT dc.customer_key) * 100 AS percent_customers_ordered,
+	ROUND(CAST(COUNT(DISTINCT fo.customer_key) AS FLOAT)/COUNT(DISTINCT dc.customer_key) * 100, 2) AS percent_cust_ordered,
+	(SELECT COUNT(product_key) FROM gold.dim_products) AS total_products,
+	COUNT(DISTINCT fo.product_key) AS total_product_ordered,
+	ROUND(CAST(COUNT(DISTINCT fo.product_key) AS FLOAT)/(SELECT COUNT(product_key) FROM gold.dim_products) * 100, 2) AS percent_product_ordered,
 	COUNT(DISTINCT fo.order_id) AS total_orders,
-	SUM(fo.quantity) AS total_quantity,
 	SUM(fo.sales) AS total_sales,
+	SUM(fo.quantity) AS total_quantity,
 	SUM(fo.profit) AS total_profit,
-	SUM(COALESCE(fo.quantity * fo.unit_price, 0))/NULLIF(SUM(COALESCE(fo.quantity, 0)), 0) AS weighted_avg_price,
-	AVG(fo.discount) AS avg_discount,
-	AVG(score) AS avg_score
+	ROUND(CAST(SUM(fo.unit_price * fo.quantity) AS FLOAT)/NULLIF(SUM(COALESCE(fo.quantity, 0)), 0), 2) AS weighted_avg_price,
+	ROUND(CAST(AVG(fo.discount) AS FLOAT), 2) AS avg_discount,
+	MAX(fo.discount) AS max_discount,
+	AVG(DATEDIFF(day, order_date, shipping_date)) AS avg_days_to_ship
 FROM gold.dim_customers dc
 LEFT JOIN gold.fact_orders fo
 ON dc.customer_key = fo.customer_key
 GROUP BY dc.country
-ORDER BY total_profit DESC;
+ORDER BY total_sales DESC;
 
--- Within each country, which cities are the largest contributors to profit,
--- and how do customer volume, order volume, quantity, revenue, and pricing differ?
+-- Within each country, which city brings in the most revenue
+-- and what other driving factors explain this performance?
 SELECT
 	dc.country,
 	dc.city,
 	COUNT(DISTINCT dc.customer_key) AS total_customers,
 	COUNT(DISTINCT fo.customer_key) AS total_customers_ordered,
-	(CAST(COUNT(DISTINCT fo.customer_key) AS FLOAT))/COUNT(DISTINCT dc.customer_key) * 100 AS percent_customers_ordered, 
+	ROUND(CAST(COUNT(DISTINCT fo.customer_key) AS FLOAT)/COUNT(DISTINCT dc.customer_key) * 100, 2) AS percent_cust_ordered,
+	(SELECT COUNT(product_key) FROM gold.dim_products) AS total_products,
+	COUNT(DISTINCT fo.product_key) AS total_product_ordered,
+	ROUND(CAST(COUNT(DISTINCT fo.product_key) AS FLOAT)/(SELECT COUNT(product_key) FROM gold.dim_products) * 100, 2) AS percent_product_ordered,
 	COUNT(DISTINCT fo.order_id) AS total_orders,
-	SUM(fo.quantity) AS total_quantity,
 	SUM(fo.sales) AS total_sales,
+	SUM(fo.quantity) AS total_quantity,
 	SUM(fo.profit) AS total_profit,
-	SUM(COALESCE(fo.quantity * fo.unit_price, 0))/NULLIF(SUM(COALESCE(fo.quantity, 0)), 0) AS weighted_avg_price,
-	AVG(fo.discount) AS avg_discount,
-	AVG(score) AS avg_score
+	ROUND(CAST(SUM(fo.unit_price * fo.quantity) AS FLOAT)/NULLIF(SUM(COALESCE(fo.quantity, 0)), 0), 2) AS weighted_avg_price,
+	ROUND(CAST(AVG(fo.discount) AS FLOAT), 2) AS avg_discount,
+	MAX(fo.discount) AS max_discount,
+	AVG(DATEDIFF(day, order_date, shipping_date)) AS avg_days_to_ship
 FROM gold.dim_customers dc
 LEFT JOIN gold.fact_orders fo
 ON dc.customer_key = fo.customer_key
 GROUP BY dc.country, dc.city
-ORDER BY country, total_profit DESC;
+ORDER BY dc.country, total_sales DESC;
 
--- Which delivery area brings in the most profit,
--- and what other driving factors are at play?
+-- Zooming into each country & city, which delivery area (postal code) generates the highest revenue
+-- and how do other metrics drive this?
 SELECT
 	dc.country,
 	dc.city,
 	dc.postal_code,
 	COUNT(DISTINCT dc.customer_key) AS total_customers,
 	COUNT(DISTINCT fo.customer_key) AS total_customers_ordered,
-	(CAST(COUNT(DISTINCT fo.customer_key) AS FLOAT))/COUNT(DISTINCT dc.customer_key) * 100 AS percent_customers_ordered, 
+	ROUND(CAST(COUNT(DISTINCT fo.customer_key) AS FLOAT)/COUNT(DISTINCT dc.customer_key) * 100, 2) AS percent_cust_ordered,
+	(SELECT COUNT(product_key) FROM gold.dim_products) AS total_products,
+	COUNT(DISTINCT fo.product_key) AS total_product_ordered,
+	ROUND(CAST(COUNT(DISTINCT fo.product_key) AS FLOAT)/(SELECT COUNT(product_key) FROM gold.dim_products) * 100, 2) AS percent_product_ordered,
 	COUNT(DISTINCT fo.order_id) AS total_orders,
-	SUM(fo.quantity) AS total_quantity,
 	SUM(fo.sales) AS total_sales,
+	SUM(fo.quantity) AS total_quantity,
 	SUM(fo.profit) AS total_profit,
-	SUM(COALESCE(fo.quantity * fo.unit_price, 0))/NULLIF(SUM(COALESCE(fo.quantity, 0)), 0) AS weighted_avg_price,
-	AVG(fo.discount) AS avg_discount,
-	AVG(score) AS avg_score
+	ROUND(CAST(SUM(fo.unit_price * fo.quantity) AS FLOAT)/NULLIF(SUM(COALESCE(fo.quantity, 0)), 0), 2) AS weighted_avg_price,
+	ROUND(CAST(AVG(fo.discount) AS FLOAT), 2) AS avg_discount,
+	MAX(fo.discount) AS max_discount,
+	AVG(DATEDIFF(day, order_date, shipping_date)) AS avg_days_to_ship
 FROM gold.dim_customers dc
 LEFT JOIN gold.fact_orders fo
 ON dc.customer_key = fo.customer_key
 GROUP BY dc.country, dc.city, dc.postal_code
-ORDER BY country, total_profit DESC;
+ORDER BY dc.country, dc.city, total_sales DESC;
 
-
--- Which of our customers has the highest total profit,
--- and what factors contribute to this performance?
-SELECT
-	dc.customer_key,
-	CONCAT(dc.first_name, ' ', dc.last_name) AS customer_name,
-	COUNT(DISTINCT fo.order_id) AS total_orders,
-	SUM(fo.quantity) AS total_quantity,
-	SUM(fo.sales) AS total_sales,
-	SUM(fo.profit) AS total_profit,
-	SUM(fo.quantity * fo.unit_price)/SUM(fo.quantity) AS weighted_avg_price,
-	AVG(fo.discount) AS avg_discount,
-	AVG(score) AS avg_score
-FROM gold.fact_orders fo 
-LEFT JOIN gold.dim_customers dc
-ON dc.customer_key = fo.customer_key
-GROUP BY dc.customer_key, CONCAT(dc.first_name, ' ', dc.last_name)
-ORDER BY total_profit DESC;
-
--- What category of product brings in the most profit,
--- and what factors contribute to this performance?
+-- Which category of products generates the most revenue
+-- and what other business metrics drive this?
 SELECT
 	dp.category,
+	COUNT(DISTINCT dp.product_key) AS total_products,
+	COUNT(DISTINCT fo.product_key) AS total_products_ordered,
+	ROUND(CAST(COUNT(DISTINCT fo.product_key) AS FLOAT)/COUNT(DISTINCT dp.product_key) * 100, 2) AS percent_products_ordered,
+	(SELECT COUNT(customer_key) FROM gold.dim_customers) AS total_customers,
+	COUNT(DISTINCT fo.customer_key) AS total_customers_ordered,
+	ROUND(CAST(COUNT(DISTINCT fo.customer_key) AS FLOAT)/(SELECT COUNT(customer_key) FROM gold.dim_customers) * 100, 2) AS percent_customers_ordered,
 	COUNT(DISTINCT fo.order_id) AS total_orders,
-	COUNT(DISTINCT fo.customer_key) AS total_customers,
-	SUM(fo.quantity) AS total_quantity,
 	SUM(fo.sales) AS total_sales,
+	SUM(fo.quantity) AS total_quantity,
 	SUM(fo.profit) AS total_profit,
-	AVG(fo.unit_price) AS avg_price,
-	SUM(fo.quantity * fo.unit_price)/SUM(fo.quantity) AS weighted_avg_price,
-	AVG(fo.discount) AS avg_discount
-FROM gold.fact_orders fo 
-LEFT JOIN gold.dim_products dp
+	ROUND(CAST(AVG(fo.unit_price) AS FLOAT), 2) AS avg_selling_price,
+	ROUND(CAST(SUM(fo.unit_price * fo.quantity) AS FLOAT)/NULLIF(SUM(COALESCE(fo.quantity, 0)), 0), 2) AS weighted_avg_price,
+	ROUND(CAST(AVG(fo.discount) AS FLOAT), 2) AS avg_discount,
+	MAX(fo.discount) AS max_discount,
+	AVG(DATEDIFF(day, order_date, shipping_date)) AS avg_days_to_ship
+FROM gold.dim_products dp
+LEFT JOIN gold.fact_orders fo
 ON dp.product_key = fo.product_key
 GROUP BY dp.category
-ORDER BY total_profit DESC;
+ORDER BY total_sales DESC;
 
--- Within each category, which sub category of product has the highest total profit,
--- and what other metrics played a substantial role in this performance?
+-- Within each category of products, which sub category brings in the most revenue
+-- and what other factors drive this performance?
 SELECT
 	dp.category,
 	dp.sub_category,
+	COUNT(DISTINCT dp.product_key) AS total_products,
+	COUNT(DISTINCT fo.product_key) AS total_products_ordered,
+	ROUND(CAST(COUNT(DISTINCT fo.product_key) AS FLOAT)/COUNT(DISTINCT dp.product_key) * 100, 2) AS percent_products_ordered,
+	(SELECT COUNT(customer_key) FROM gold.dim_customers) AS total_customers,
+	COUNT(DISTINCT fo.customer_key) AS total_customers_ordered,
+	ROUND(CAST(COUNT(DISTINCT fo.customer_key) AS FLOAT)/(SELECT COUNT(customer_key) FROM gold.dim_customers) * 100, 2) AS percent_customers_ordered,
 	COUNT(DISTINCT fo.order_id) AS total_orders,
-	COUNT(DISTINCT fo.customer_key) AS total_customers,
-	SUM(fo.quantity) AS total_quantity,
 	SUM(fo.sales) AS total_sales,
+	SUM(fo.quantity) AS total_quantity,
 	SUM(fo.profit) AS total_profit,
-	AVG(fo.unit_price) AS avg_price,
-	SUM(fo.quantity * fo.unit_price)/SUM(fo.quantity) AS weighted_avg_price,
-	AVG(fo.discount) AS avg_discount
-FROM gold.fact_orders fo 
-LEFT JOIN gold.dim_products dp
+	ROUND(CAST(AVG(fo.unit_price) AS FLOAT), 2) AS avg_selling_price,
+	ROUND(CAST(SUM(fo.unit_price * fo.quantity) AS FLOAT)/NULLIF(SUM(COALESCE(fo.quantity, 0)), 0), 2) AS weighted_avg_price,
+	ROUND(CAST(AVG(fo.discount) AS FLOAT), 2) AS avg_discount,
+	MAX(fo.discount) AS max_discount,
+	AVG(DATEDIFF(day, order_date, shipping_date)) AS avg_days_to_ship
+FROM gold.dim_products dp
+LEFT JOIN gold.fact_orders fo
 ON dp.product_key = fo.product_key
 GROUP BY dp.category, dp.sub_category
-ORDER BY category, total_profit DESC;
+ORDER BY dp.category, total_sales DESC;
 
--- Within each category & sub category, which product brings in the most profit,
--- and how do metrics like order volume, total customers, revenue, pricing, and discount contribute to this?
+-- Within each category & sub category, which product generates the most revenue
+-- and what other driving factors are at play?
 SELECT
 	dp.category,
 	dp.sub_category,
 	dp.product_name,
+	(SELECT COUNT(customer_key) FROM gold.dim_customers) AS total_customers,
+	COUNT(DISTINCT fo.customer_key) AS total_customers_ordered,
+	ROUND(CAST(COUNT(DISTINCT fo.customer_key) AS FLOAT)/(SELECT COUNT(customer_key) FROM gold.dim_customers) * 100, 2) AS percent_customers_ordered,
 	COUNT(DISTINCT fo.order_id) AS total_orders,
-	COUNT(DISTINCT fo.customer_key) AS total_customers,
-	SUM(fo.quantity) AS total_quantity,
 	SUM(fo.sales) AS total_sales,
+	SUM(fo.quantity) AS total_quantity,
 	SUM(fo.profit) AS total_profit,
-	AVG(fo.unit_price) AS avg_price,
-	AVG(fo.discount) AS avg_discount
+	ROUND(CAST(AVG(fo.unit_price) AS FLOAT), 2) AS avg_selling_price,
+	ROUND(CAST(SUM(fo.unit_price * fo.quantity) AS FLOAT)/NULLIF(SUM(COALESCE(fo.quantity, 0)), 0), 2) AS weighted_avg_price,
+	ROUND(CAST(AVG(fo.discount) AS FLOAT), 2) AS avg_discount,
+	MAX(fo.discount) AS max_discount,
+	AVG(DATEDIFF(day, order_date, shipping_date)) AS avg_days_to_ship
 FROM gold.dim_products dp
-LEFT JOIN gold.fact_orders fo
+INNER JOIN gold.fact_orders fo
 ON dp.product_key = fo.product_key
 GROUP BY dp.category, dp.sub_category, dp.product_name
-ORDER BY dp.category, dp.sub_category, total_profit DESC;
+ORDER BY dp.category, dp.sub_category, total_sales DESC;
+
+-- Irrespective of category & sub category, what are the most revenue-generating products, and why?
+SELECT
+	dp.product_name,
+	(SELECT COUNT(customer_key) FROM gold.dim_customers) AS total_customers,
+	COUNT(DISTINCT fo.customer_key) AS total_customers_ordered,
+	ROUND(CAST(COUNT(DISTINCT fo.customer_key) AS FLOAT)/(SELECT COUNT(customer_key) FROM gold.dim_customers) * 100, 2) AS percent_customers_ordered,
+	COUNT(DISTINCT fo.order_id) AS total_orders,
+	SUM(fo.sales) AS total_sales,
+	SUM(fo.quantity) AS total_quantity,
+	SUM(fo.profit) AS total_profit,
+	ROUND(CAST(AVG(fo.unit_price) AS FLOAT), 2) AS avg_selling_price,
+	ROUND(CAST(SUM(fo.unit_price * fo.quantity) AS FLOAT)/NULLIF(SUM(COALESCE(fo.quantity, 0)), 0), 2) AS weighted_avg_price,
+	ROUND(CAST(AVG(fo.discount) AS FLOAT), 2) AS avg_discount,
+	MAX(fo.discount) AS max_discount,
+	AVG(DATEDIFF(day, order_date, shipping_date)) AS avg_days_to_ship
+FROM gold.dim_products dp
+INNER JOIN gold.fact_orders fo
+ON dp.product_key = fo.product_key
+GROUP BY dp.product_name
+ORDER BY total_sales DESC;
+
+-- Within each country, which category of product brings in most of the revenue, and why?
+SELECT
+	dc.country,
+	dp.category,
+	MAX(tcc.total_customers_country) AS total_customers_country,
+	COUNT(DISTINCT fo.customer_key) AS total_customers_ordered,
+	ROUND(CAST(COUNT(DISTINCT fo.customer_key) AS FLOAT)/MAX(tcc.total_customers_country), 2) * 100 AS percent_customers_ordered,
+	MAX(tpc.total_products_category) AS total_products_category,
+	COUNT(DISTINCT fo.product_key) AS total_products_ordered,
+	ROUND(CAST(COUNT(DISTINCT fo.product_key) AS FLOAT)/MAX(tpc.total_products_category) * 100, 2) AS percent_products_ordered,
+	COUNT(DISTINCT fo.order_id) AS total_orders,
+	SUM(fo.sales) AS total_sales,
+	SUM(fo.quantity) AS total_quantity,
+	SUM(fo.profit) AS total_profit,
+	ROUND(CAST(SUM(fo.unit_price * fo.quantity) AS FLOAT)/NULLIF(SUM(COALESCE(fo.quantity, 0)), 0), 2) AS weighted_avg_price,
+	ROUND(CAST(AVG(fo.discount) AS FLOAT), 2) AS avg_discount,
+	MAX(fo.discount) AS max_discount,
+	AVG(DATEDIFF(day, order_date, shipping_date)) AS avg_days_to_ship
+FROM gold.dim_customers dc
+INNER JOIN 
+(
+	SELECT 
+		country, 
+		COUNT(customer_key) AS total_customers_country 
+	FROM gold.dim_customers GROUP BY country
+)tcc
+ON dc.country = tcc.country
+INNER JOIN gold.fact_orders fo
+ON dc.customer_key = fo.customer_key
+INNER JOIN gold.dim_products dp
+ON fo.product_key = dp.product_key
+INNER JOIN
+(
+	SELECT 
+		category, 
+		COUNT(product_key) AS total_products_category 
+	FROM gold.dim_products 
+	GROUP BY category
+)tpc
+ON dp.category = tpc.category
+GROUP BY dc.country, dp.category
+ORDER BY country, total_sales DESC;
+
+-- What is currently our best year revenue-wise?
+SELECT
+	YEAR(fo.order_date) AS order_date_year,
+	COUNT(DISTINCT fo.customer_key) AS total_customers_ordered,
+	COUNT(DISTINCT fo.product_key) AS total_products_ordered,
+	COUNT(DISTINCT fo.order_id) AS total_orders,
+	SUM(fo.sales) AS total_sales,
+	SUM(fo.quantity) AS total_quantity,
+	SUM(fo.profit) AS total_profit,
+	ROUND(CAST(SUM(fo.unit_price * fo.quantity) AS FLOAT)/NULLIF(SUM(COALESCE(fo.quantity, 0)), 0), 2) AS weighted_avg_price,
+	ROUND(CAST(AVG(fo.discount) AS FLOAT), 2) AS avg_discount,
+	MAX(fo.discount) AS max_discount,
+	AVG(DATEDIFF(day, order_date, shipping_date)) AS avg_days_to_ship
+FROM gold.dim_customers dc
+LEFT JOIN gold.fact_orders fo
+ON dc.customer_key = fo.customer_key
+GROUP BY YEAR(order_date)
+ORDER BY total_sales DESC;
+
+-- Within each year, which month brings in the most revenue?
+SELECT
+	YEAR(fo.order_date) AS order_date_year,
+	DATENAME(month, fo.order_date) AS month_name,
+	COUNT(DISTINCT fo.customer_key) AS total_customers_ordered,
+	COUNT(DISTINCT fo.product_key) AS total_products_ordered,
+	COUNT(DISTINCT fo.order_id) AS total_orders,
+	SUM(fo.sales) AS total_sales,
+	SUM(fo.quantity) AS total_quantity,
+	SUM(fo.profit) AS total_profit,
+	ROUND(CAST(SUM(fo.unit_price * fo.quantity) AS FLOAT)/NULLIF(SUM(COALESCE(fo.quantity, 0)), 0), 2) AS weighted_avg_price,
+	ROUND(CAST(AVG(fo.discount) AS FLOAT), 2) AS avg_discount,
+	MAX(fo.discount) AS max_discount,
+	AVG(DATEDIFF(day, order_date, shipping_date)) AS avg_days_to_ship
+FROM gold.dim_customers dc
+LEFT JOIN gold.fact_orders fo
+ON dc.customer_key = fo.customer_key
+GROUP BY YEAR(order_date), DATENAME(month, order_date)
+ORDER BY order_date_year DESC, total_sales DESC;
+
+
+-- Is our best year consistent across all countries?
+SELECT
+	dc.country,
+	YEAR(fo.order_date) AS order_date_year,
+	COUNT(DISTINCT fo.customer_key) AS total_customers_ordered,
+	COUNT(DISTINCT fo.product_key) AS total_products_ordered,
+	COUNT(DISTINCT fo.order_id) AS total_orders,
+	SUM(fo.sales) AS total_sales,
+	SUM(fo.quantity) AS total_quantity,
+	SUM(fo.profit) AS total_profit,
+	ROUND(CAST(SUM(fo.unit_price * fo.quantity) AS FLOAT)/NULLIF(SUM(COALESCE(fo.quantity, 0)), 0), 2) AS weighted_avg_price,
+	ROUND(CAST(AVG(fo.discount) AS FLOAT), 2) AS avg_discount,
+	MAX(fo.discount) AS max_discount,
+	AVG(DATEDIFF(day, order_date, shipping_date)) AS avg_days_to_ship
+FROM gold.dim_customers dc
+INNER JOIN gold.fact_orders fo
+ON dc.customer_key = fo.customer_key
+GROUP BY dc.country, YEAR(fo.order_date)
+ORDER BY country, total_sales DESC;
+
+-- Which our customers generate the highest revenue?
+SELECT
+	customer_key,
+	(SELECT COUNT(product_key) FROM gold.dim_products) AS total_products,
+	COUNT(DISTINCT product_key) AS total_product_ordered,
+	ROUND(CAST(COUNT(DISTINCT product_key) AS FLOAT)/(SELECT COUNT(product_key) FROM gold.dim_products) * 100, 2) AS percent_product_ordered,
+	COUNT(DISTINCT order_id) AS total_orders,
+	SUM(sales) AS total_sales,
+	SUM(quantity) AS total_quantity,
+	SUM(profit) AS total_profit,
+	ROUND(CAST(SUM(unit_price * quantity) AS FLOAT)/NULLIF(SUM(COALESCE(quantity, 0)), 0), 2) AS weighted_avg_price,
+	ROUND(CAST(AVG(discount) AS FLOAT), 2) AS avg_discount,
+	MAX(discount) AS max_discount
+FROM gold.fact_orders
+GROUP BY customer_key
+ORDER BY total_sales DESC;
