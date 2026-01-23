06712bd2c3f1c75f555b6b29ee3de37de3fba186
Toyib Otusanya <toyibotusanya@gmail.com>
2026-01-23 16:36:45 +0000

    Update 03_measures_exploration.sql

--- scripts(data analytics)/01_Exploratory_data_analytics/03_measures_exploration.sql
+++ scripts(data analytics)/01_Exploratory_data_analytics/03_measures_exploration.sql
@@ -1,68 +1,73 @@
 /*
-=======================================================================================
-Measures Exploration
-=======================================================================================
+========================================================================
+Measure Exploration
+========================================================================
 Script Purpose:
-	This script explores key business metrics, giving an overview of the business
-	performance.
-=======================================================================================
+	This script explores key business metrics, giving an overview of 
+	the business performance.
+========================================================================
 */
--- How much revenue has been generated within the span of 4 years?
+-- What is the total revenue generated?
 SELECT SUM(sales) AS total_sales FROM gold.fact_orders;
 
--- How many quantity of products were sold to generate this revenue?
+-- How many quantities of product were sold to generate such revenue?
 SELECT SUM(quantity) AS total_quantity FROM gold.fact_orders;
 
 -- How many orders generated this revenue?
 SELECT COUNT(DISTINCT order_id) AS total_orders FROM gold.fact_orders;
 
--- What is the total profit made from this revenue?
+-- What is the total profit generated from this revenue?
 SELECT SUM(profit) AS total_profit FROM gold.fact_orders;
 
--- What is the average selling price of our products?
+-- What is the average selling price?
 SELECT AVG(unit_price) AS avg_selling_price FROM gold.fact_orders;
 
--- What is the highest price?
-SELECT MAX(unit_price) AS highest_price FROM gold.fact_orders;
+-- What is the highest selling price?
+SELECT MAX(unit_price) AS highest_selling_price FROM gold.fact_orders;
 
--- What is the lowest price?
-SELECT MIN(unit_price) AS lowest_price FROM gold.fact_orders;
+-- What is the lowest selling price?
+SELECT MIN(unit_price) AS lowest_selling_price FROM gold.fact_orders;
 
 -- What is the average discount?
 SELECT AVG(discount) AS avg_discount FROM gold.fact_orders;
 
--- What is the highest discount?
+-- What is the higest discount?
 SELECT MAX(discount) AS highest_discount FROM gold.fact_orders;
 
 -- What is the lowest discount?
 SELECT MIN(discount) AS lowest_discount FROM gold.fact_orders;
 
--- What is the average shipping days
+-- What is the average days to ship?
 SELECT AVG(DATEDIFF(day, order_date, shipping_date)) AS avg_days_to_ship FROM gold.fact_orders;
 
--- How many customers do we have?
+-- What is the highest number of days to ship?
+SELECT MAX(DATEDIFF(day, order_date, shipping_date)) AS highest_days_to_ship FROM gold.fact_orders;
+
+-- What is the lowest number of days to ships?
+SELECT MIN(DATEDIFF(day, order_date, shipping_date)) AS lowest_days_to_ship FROM gold.fact_orders;
+
+-- How many customers does the company have?
 SELECT COUNT(customer_key) AS total_customers FROM gold.dim_customers;
 
 -- How many of these customers have ordered?
 SELECT COUNT(DISTINCT customer_key) AS total_customers_ordered FROM gold.fact_orders;
 
--- What is the avearge customer score?
-SELECT AVG(score) AS avg_score FROM gold.dim_customers;
+-- How many have been ordered this year?
+SELECT COUNT(DISTINCT customer_key) AS total_customers_ordered_recent FROM gold.fact_orders
+WHERE YEAR(order_date) = YEAR(GETDATE());
 
--- What is the highest score?
-SELECT MAX(score) AS highest_score FROM gold.dim_customers;
-
--- What is the lowest score?
-SELECT MIN(score) AS lowest_score FROM gold.dim_customers;
-
--- How many products do we have?
+-- How many unique products does the business have?
 SELECT COUNT(product_key) AS total_products FROM gold.dim_products;
 
 -- How many of these products have been ordered?
 SELECT COUNT(DISTINCT product_key) AS total_products_ordered FROM gold.fact_orders;
 
+-- How many have been ordered this year?
+SELECT COUNT(DISTINCT product_key) AS total_products_ordered_recent FROM gold.fact_orders
+WHERE YEAR(order_date) = YEAR(GETDATE());
 
--- Report consolidating all business metrics
+
+-- Generate a report consolidating key business metrics
 SELECT 'Total Sales' AS measure_name, SUM(sales) AS measure_value FROM gold.fact_orders
 UNION ALL
 SELECT 'Total Quantity', SUM(quantity) FROM gold.fact_orders
@@ -83,18 +88,22 @@ SELECT 'Highest Discount', MAX(discount) FROM gold.fact_orders
 UNION ALL
 SELECT 'Lowest Discount', MIN(discount) FROM gold.fact_orders
 UNION ALL
-SELECT 'Average Days to Ship', AVG(DATEDIFF(day, order_date, shipping_date)) FROM gold.fact_orders
+SELECT 'Avg Days to Ship', AVG(DATEDIFF(day, order_date, shipping_date)) FROM gold.fact_orders
+UNION ALL
+SELECT 'Highest Shipping Days', MAX(DATEDIFF(day, order_date, shipping_date)) FROM gold.fact_orders
+UNION ALL
+SELECT 'Lowest Shipping Days', MIN(DATEDIFF(day, order_date, shipping_date)) FROM gold.fact_orders
 UNION ALL
 SELECT 'Total Customers', COUNT(customer_key) FROM gold.dim_customers
 UNION ALL
 SELECT 'Total Customers Ordered', COUNT(DISTINCT customer_key) FROM gold.fact_orders
 UNION ALL
-SELECT 'Average Score', AVG(score) FROM gold.dim_customers
-UNION ALL
-SELECT 'Highest Score', MAX(score) FROM gold.dim_customers
-UNION ALL
-SELECT 'Lowest Score', MIN(score) FROM gold.dim_customers
+SELECT 'Total Customers Ordered this Year', COUNT(DISTINCT customer_key) FROM gold.fact_orders
+WHERE YEAR(order_date) = YEAR(GETDATE())
 UNION ALL
 SELECT 'Total Products', COUNT(product_key) FROM gold.dim_products
 UNION ALL
-SELECT 'Total Products Ordered', COUNT(DISTINCT product_key) FROM gold.fact_orders;
+SELECT 'Total Products Ordered', COUNT(DISTINCT product_key) FROM gold.fact_orders
+UNION ALL
+SELECT 'Total Products Ordered this Year', COUNT(DISTINCT product_key) FROM gold.fact_orders
+WHERE YEAR(order_date) = YEAR(GETDATE());
