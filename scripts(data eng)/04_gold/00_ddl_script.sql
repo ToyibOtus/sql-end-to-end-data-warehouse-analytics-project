f73d8446ac8b9067a82da8f799b8e69263c46831
Toyib Otusanya <toyibotusanya@gmail.com>
2026-02-07 07:16:34 +0000

    Modify sales columns in fact_orders table
    Updated data types for sales-related columns in fact_orders table.

--- scripts(data eng)/04_gold/00_ddl_script.sql
+++ scripts(data eng)/04_gold/00_ddl_script.sql
@@ -9,7 +9,16 @@ Script Purpose:
 */
 USE SalesDatabase;
 GO
-	
+
+DROP TABLE IF EXISTS gold.fact_orders;
+DROP TABLE IF EXISTS gold.dim_customers;
+DROP TABLE IF EXISTS gold.dim_products;
+
+-- Drop table [gold.dim_customers] if exists
+IF OBJECT_ID('gold.dim_customers', 'U') IS NOT NULL
+DROP TABLE gold.dim_customers;
+GO
+
 -- Create table [gold.dim_customers]
 CREATE TABLE gold.dim_customers
 (
@@ -23,6 +32,11 @@ CREATE TABLE gold.dim_customers
 	score INT
 );
 
+-- Drop table [gold.dim_products] if exists
+IF OBJECT_ID('gold.dim_products', 'U') IS NOT NULL
+DROP TABLE gold.dim_products;
+GO
+
 -- Create table [gold.dim_products]
 CREATE TABLE gold.dim_products
 (
@@ -33,6 +47,11 @@ CREATE TABLE gold.dim_products
 	sub_category NVARCHAR(50) NOT NULL
 );
 
+-- Drop table [gold.fact_orders] if exists
+IF OBJECT_ID('gold.fact_orders', 'U') IS NOT NULL
+DROP TABLE gold.fact_orders;
+GO
+
 -- Create table [gold.fact_orders]
 CREATE TABLE gold.fact_orders
 (
@@ -41,11 +60,12 @@ CREATE TABLE gold.fact_orders
 	product_key INT NOT NULL,
 	order_date DATE NOT NULL,
 	shipping_date DATE,
-	sales DECIMAL(10, 3) NOT NULL,
+	gross_sales DECIMAL(10, 2) NOT NULL,
+	net_sales DECIMAL(10, 2) NOT NULL,
 	quantity INT NOT NULL,
 	discount DECIMAL(10, 2) NOT NULL,
-	profit DECIMAL(10, 3) NOT NULL,
-	unit_price DECIMAL(10, 3) NOT NULL,
+	profit DECIMAL(10, 2) NOT NULL,
+	unit_price DECIMAL(10, 2) NOT NULL,
 	CONSTRAINT uq_order_id_product_key UNIQUE (order_id, product_key),
 	CONSTRAINT fk_gold_fact_orders_customer_key FOREIGN KEY(customer_key) REFERENCES gold.dim_customers(customer_key),
 	CONSTRAINT fk_gold_fact_orders_product_key FOREIGN KEY(product_key) REFERENCES gold.dim_products(product_key)
